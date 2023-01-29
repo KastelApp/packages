0a88cd4d354ee71e234333c395ff3dba2c41bdd6
DarkInk <darkerink@zenaxis.org>
2023-01-29 11:44:28 -0600

    Error Handler

---
+++ lib/__tests__/Errors.tests.ts
@@ -0,0 +1,84 @@
+import HTTPErrors from '../Errors';
+
+test('ErrorFromConstructor', () => {
+    const error = new HTTPErrors(5251, {
+        embeds: [
+            {
+                description: {
+                    code: 'DESCRIPTION_TOO_LONG',
+                    message: 'Description is too long',
+                },
+            },
+        ],
+    });
+
+    expect(error.code).toBe(5251);
+
+    expect(error.errors).toEqual({
+        'embeds': {
+            '0': {
+                'description': {
+                    'code': 'DESCRIPTION_TOO_LONG',
+                    'message': 'Description is too long',
+                },
+            },
+        },
+    });
+
+});
+
+test('ErrorFromAddError', () => {
+    const error = new HTTPErrors(5251);
+
+    error.addError({
+        embeds: [
+            {
+                description: {
+                    code: 'DESCRIPTION_TOO_LONG',
+                    message: 'Description is too long',
+                },
+            },
+        ],
+    });
+
+    expect(error.code).toBe(5251);
+
+    expect(error.errors).toEqual({
+        'embeds': {
+            '0': {
+                'description': {
+                    'code': 'DESCRIPTION_TOO_LONG',
+                    'message': 'Description is too long',
+                },
+            },
+        },
+    });
+
+});
+
+test('ErrorFromAddToError', () => {
+    const error = new HTTPErrors(5251);
+
+    error.addToError('embeds', {
+        0: {
+            description: {
+                code: 'DESCRIPTION_TOO_LONG',
+                message: 'Description is too long',
+            },
+        },
+    });
+
+    expect(error.code).toBe(5251);
+
+    expect(error.errors).toEqual({
+        'embeds': {
+            '0': {
+                'description': {
+                    'code': 'DESCRIPTION_TOO_LONG',
+                    'message': 'Description is too long',
+                },
+            },
+        },
+    });
+
+});
