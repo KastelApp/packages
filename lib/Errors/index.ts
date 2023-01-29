4b66e33a01c1fa5a964900d69787bc4a5e4a8c34
DarkInk <darkerink@zenaxis.org>
2023-01-29 11:45:12 -0600

    2.2.0

--- lib/Errors/index.ts
+++ lib/Errors/index.ts
@@ -1,75 +1,75 @@
 class HTTPErrors {
-    code: number;
-    errors: { [key: string]: any };
+  code: number;
+  errors: { [key: string]: any };
 
-    constructor(code: number, ...errors: any[]) {
-        this.code = code;
-        this.errors = {};
+  constructor(code: number, ...errors: any[]) {
+    this.code = code;
+    this.errors = {};
 
-        for (const error of errors) {
-            this.addError(error);
-        }
+    for (const error of errors) {
+      this.addError(error);
     }
+  }
 
-    addError(error: { [key: string]: any }): void {
-        Object.entries(error).forEach(([key, value]) => {
-            if (Array.isArray(value)) {
-                value.forEach((item, index) => {
-                    if (this.errors[key]) {
-                        this.errors[key][index] = item;
-                    } else {
-                        this.errors[key] = { [index]: item };
-                    }
-                });
-            } else {
-                this.errors[key] = value;
-            }
+  addError(error: { [key: string]: any }): void {
+    Object.entries(error).forEach(([key, value]) => {
+      if (Array.isArray(value)) {
+        value.forEach((item, index) => {
+          if (this.errors[key]) {
+            this.errors[key][index] = item;
+          } else {
+            this.errors[key] = { [index]: item };
+          }
         });
-    }
+      } else {
+        this.errors[key] = value;
+      }
+    });
+  }
 
-    addToError(errorName: string, error: { [key: string]: any }): void {
-        Object.entries(error).forEach(([, value]) => {
-            if (Array.isArray(value)) {
-                value.forEach((item, index) => {
-                    if (this.errors[errorName]) {
-                        this.errors[errorName][index] = item;
-                    } else {
-                        this.errors[errorName] = { [index]: item };
-                    }
-                });
-            } else {
-                Object.entries(value).forEach(([keyf, valuef]) => {
-                    if (this.errors[errorName]) {
-                        const index = Object.keys(this.errors[errorName]).length;
-                        this.errors[errorName][index] = { [keyf]: valuef };
-                    } else {
-                        this.errors[errorName] = {
-                            0: { [keyf]: valuef },
-                        };
-                    }
-                });
-            }
+  addToError(errorName: string, error: { [key: string]: any }): void {
+    Object.entries(error).forEach(([, value]) => {
+      if (Array.isArray(value)) {
+        value.forEach((item, index) => {
+          if (this.errors[errorName]) {
+            this.errors[errorName][index] = item;
+          } else {
+            this.errors[errorName] = { [index]: item };
+          }
         });
-    }
+      } else {
+        Object.entries(value).forEach(([keyf, valuef]) => {
+          if (this.errors[errorName]) {
+            const index = Object.keys(this.errors[errorName]).length;
+            this.errors[errorName][index] = { [keyf]: valuef };
+          } else {
+            this.errors[errorName] = {
+              0: { [keyf]: valuef },
+            };
+          }
+        });
+      }
+    });
+  }
 
-    toJSON(): { code: number; errors: { [key: string]: any } } {
-        return {
-            code: this.code,
-            errors: this.errors,
-        };
-    }
+  toJSON(): { code: number; errors: { [key: string]: any } } {
+    return {
+      code: this.code,
+      errors: this.errors,
+    };
+  }
 
-    toString(): string {
-        return JSON.stringify(this.toJSON());
-    }
+  toString(): string {
+    return JSON.stringify(this.toJSON());
+  }
 
-    clearErrors(): void {
-        this.errors = {};
-    }
+  clearErrors(): void {
+    this.errors = {};
+  }
 
-    clearError(errorName: string): void {
-        delete this.errors[errorName];
-    }
+  clearError(errorName: string): void {
+    delete this.errors[errorName];
+  }
 }
 
 export default HTTPErrors;
--- lib/__tests__/Errors.tests.ts
+++ lib/__tests__/Errors.tests.ts
@@ -1,84 +1,81 @@
 import HTTPErrors from '../Errors';
 
 test('ErrorFromConstructor', () => {
-    const error = new HTTPErrors(5251, {
-        embeds: [
-            {
-                description: {
-                    code: 'DESCRIPTION_TOO_LONG',
-                    message: 'Description is too long',
-                },
-            },
-        ],
-    });
-
-    expect(error.code).toBe(5251);
-
-    expect(error.errors).toEqual({
-        'embeds': {
-            '0': {
-                'description': {
-                    'code': 'DESCRIPTION_TOO_LONG',
-                    'message': 'Description is too long',
-                },
-            },
+  const error = new HTTPErrors(5251, {
+    embeds: [
+      {
+        description: {
+          code: 'DESCRIPTION_TOO_LONG',
+          message: 'Description is too long',
         },
-    });
+      },
+    ],
+  });
 
+  expect(error.code).toBe(5251);
+
+  expect(error.errors).toEqual({
+    embeds: {
+      '0': {
+        description: {
+          code: 'DESCRIPTION_TOO_LONG',
+          message: 'Description is too long',
+        },
+      },
+    },
+  });
 });
 
 test('ErrorFromAddError', () => {
-    const error = new HTTPErrors(5251);
+  const error = new HTTPErrors(5251);
 
-    error.addError({
-        embeds: [
-            {
-                description: {
-                    code: 'DESCRIPTION_TOO_LONG',
-                    message: 'Description is too long',
-                },
-            },
-        ],
-    });
-
-    expect(error.code).toBe(5251);
-
-    expect(error.errors).toEqual({
-        'embeds': {
-            '0': {
-                'description': {
-                    'code': 'DESCRIPTION_TOO_LONG',
-                    'message': 'Description is too long',
-                },
-            },
+  error.addError({
+    embeds: [
+      {
+        description: {
+          code: 'DESCRIPTION_TOO_LONG',
+          message: 'Description is too long',
         },
-    });
+      },
+    ],
+  });
 
+  expect(error.code).toBe(5251);
+
+  expect(error.errors).toEqual({
+    embeds: {
+      '0': {
+        description: {
+          code: 'DESCRIPTION_TOO_LONG',
+          message: 'Description is too long',
+        },
+      },
+    },
+  });
 });
 
 test('ErrorFromAddToError', () => {
-    const error = new HTTPErrors(5251);
+  const error = new HTTPErrors(5251);
 
-    error.addToError('embeds', {
-        0: {
-            description: {
-                code: 'DESCRIPTION_TOO_LONG',
-                message: 'Description is too long',
-            },
+  error.addToError('embeds', {
+    0: {
+      description: {
+        code: 'DESCRIPTION_TOO_LONG',
+        message: 'Description is too long',
+      },
+    },
+  });
+
+  expect(error.code).toBe(5251);
+
+  expect(error.errors).toEqual({
+    embeds: {
+      '0': {
+        description: {
+          code: 'DESCRIPTION_TOO_LONG',
+          message: 'Description is too long',
         },
-    });
-
-    expect(error.code).toBe(5251);
-
-    expect(error.errors).toEqual({
-        'embeds': {
-            '0': {
-                'description': {
-                    'code': 'DESCRIPTION_TOO_LONG',
-                    'message': 'Description is too long',
-                },
-            },
-        },
-    });
-
+      },
+    },
+  });
 });
